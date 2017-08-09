caf53fbfbf6d9f7e9f3a3074930ba8ab2f3f6227
ZhZy <zzylxxy@163.com>
2017-08-09 09:44:12 +0800

    1.0.-

---
+++ public/test.js
@@ -0,0 +1,14 @@
+// export var name='zy';
+// export function changeName(change){
+//   name=change;
+// }
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.changeName = changeName;
+var name = exports.name = 'zy';
+function changeName(change) {
+  exports.name = name = change;
+}
